749feb3a900c297e69db9b443ed5c152dd07308c
thechutrain <chu.chuyalan@gmail.com>
2016-11-13 00:24:18 -0500

    linked js file for game, aand working object

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// console.log("This is a test!");
+
+/*
+HangMan game
+*/
+var hangmanGame = {
+  wins: 1,
+  loses: 0,
+  userGuesses: 0,
+  guessesAllowed: 7,
+  currentWord: "John Adams",
+  incorrectLetters: ["b", "c"],
+  correctLetters: [],
+
+  guessesLeft: function(){
+    console.log(this.guessesAllowed - this.userGuesses);
+  }
+
+}
