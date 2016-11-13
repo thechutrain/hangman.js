1c4e09bcb989d38aff3565011203192ba5df8f53
thechutrain <chu.chuyalan@gmail.com>
2016-11-13 13:18:27 -0500

    event listener for key up checks if game is over, if the key is a valid letter, if its been guessed already or not, and if its correct or not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 // console.log("This is a test!");
 
 /*
-hangman Model
-====================
+============================================================
+Model
+============================================================
 1.) keeps track of overall game stats
 2.) keeps track of word
 3.) keeps track of user guesses
@@ -13,7 +14,7 @@ var model = {
   wins: 1,
   loses: 0,
 
-  userGuesses: 0,
+  totalGuesses: 0,
   guessesAllowed: 7,
   gameOver: false,
 
@@ -21,43 +22,97 @@ var model = {
   currentWord: "John Adams",
   incorrectLetters: ["b", "c"],
   correctLetters: [],
+  lettersGuessed: ["r"],
 
   // METHODS
   guessesLeft: function(){
-    console.log(this.guessesAllowed - this.userGuesses);
+    console.log(this.guessesAllowed - this.totalGuesses);
   },
 
   // checks to see if the game is over or not 
   isGameOver: function(){
-    if ((this.guessesAllowed - this.userGuesses) == 0){
+    if ((this.guessesAllowed - this.totalGuesses) == 0){
       this.gameOver = true;
-      console.log("Game over!");
-    } else{
-      console.log("GAME IS NOT OVER!");
     }
-  }
+  },
 
 }
 
 /*
-Hangman View
-====================
+============================================================
+View
+============================================================
 1.) will update the keyboard
 2.) send message to user
 */
 
 
 /*
-Code snippets:
+============================================================
+Controller
+============================================================
+1.) Will contain the event listeners
+2.) Will tell the model to update itself
+3.) Will tell the view to update itself
 */
 
 // Make an Event Listener that will wait  
-document.addEventListener("keyup", function(event){
-  // Check if the user is
+// 2 ways to make an event listener
+// document.onkeyup = function(event){
+//   console.log(event.key);
+// };
 
-  // get the user's guess
-  console.log(event);
-  // 
+document.addEventListener("keyup", function(event){
+  // Part1: Check to see if the game is not over
+  model.isGameOver();
+  if (model.gameOver){
+    console.log("Game is over, play again?");
+    return;
+  }
+
+  // TO DO: Play again?
+  // write code if user presses enter, you reset game!
+
+  // Part 2: GAME is not over, so lets check what the user guessed!
+  // get the key that the user pressed
+  // console.log(event.key);
+  var userGuess = event.key;
+
+  // CHECKS ON THE EVENT
+  // 1.) check that its a letter
+  if (event.keyCode < 65 || event.keyCode > 90){
+    console.log("Sorry, " + event.key +" is not a valid letter");
+    // TO DO: alert user to only pick letters :)
+    return;
+  } 
+  // else{
+  //   console.log(event.key + " is a letter!");
+  // }
+
+  // 2.) check that it has not been guessed yet
+  if (model.lettersGuessed.indexOf(userGuess)!=-1){
+    console.log(userGuess + " has already been guessed");
+    return;
+  };
+
+  // 3.) check if it's correct
+    // 3a.) if correct --> update view, & update game
+  if (model.currentWord.indexOf(userGuess)!=-1){
+    console.log(userGuess + " is in the currentWord: " + model.currentWord);
+  } else{
+    // 3b.) if its not correct --> update view
+    console.log(userGuess + " is NOT in the currentWord: " + model.currentWord);
+  };
+
+
+
+  // check if the letter has been guessed already 
+  // check to see if user's guess is correct
+
+    // if guess is correct, add letters to board
+
+    //}
+    
 
 
 })
