e122eb2ad2cae0eabbd4e1200ce1c1b69154248f
thechutrain <chu.chuyalan@gmail.com>
2016-11-13 15:40:47 -0500

    simple view object that sends an updated message to DOM, and psuedo code for controller to better compartmentalize the js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,25 +14,27 @@ var model = {
   totalWins: 1,
   totalLoses: 0,
 
-  totalGuesses: 0,
+  incorrectGuesses: 0,
   guessesAllowed: 7,
   freezeGame: false,
   userWon: false,
 
 
   currentWord: "John Adams",
-  incorrectLetters: ["b", "c"],
-  correctLetters: [],
+  currentWordArray: [],
   lettersGuessed: [],
+  // incorrectLetters: ["b", "c"],
+  // correctLetters: [],
+
 
   // METHODS
   guessesLeft: function(){
-    console.log(this.guessesAllowed - this.totalGuesses);
+    console.log(this.guessesAllowed - this.incorrectGuesses);
   },
 
   // checks to see if the game is over or not 
   isGameDone: function(){
-    if ((this.guessesAllowed - this.totalGuesses) == 0){
+    if ((this.guessesAllowed - this.incorrectGuesses) == 0){
       this.freezeGame = true;
     };
     // TO DO: check if user won game or not
@@ -48,9 +50,15 @@ var model = {
 ============================================================
 View
 ============================================================
-1.) will update the keyboard
+1.) will update the keyboard GUI
 2.) send message to user
 */
+var view = {
+  messageElement: document.getElementById("messages"),
+  updateMessage: function(messageString){
+   this.messageElement.innerHTML = messageString;
+  }
+}
 
 
 /*
@@ -61,6 +69,49 @@ Controller
 2.) Will tell the model to update itself
 3.) Will tell the view to update itself
 */
+var controller = {
+  // This takes an keyup event
+  keyupEventListener: function(event){
+    //* 0.) update game & get the pressed key
+    // models.update()
+
+    // 1.) if game is frozen
+      // 1a.) if player won 
+        //--> update View (You won)
+
+      // 1b.) else if player did not win 
+        // --> update View (You lost)
+
+      // *EXIT!
+
+    // 2) if not valid guess
+      // 2a.) if not a letter
+          // --> update View (Not a letter)
+      // 2b.) if already picked letter
+          // --> update View (Already picked) 
+
+    // 3) else, its a valid guess and process it!
+      // 3a.) if processed guess is right!
+        // i.) update View (That's a correct letter)
+        // ii.) check if you've won --> models.update
+      // 3b.) if processed guess is incorrect
+        // i.) update View (That's a wrong letter)
+        // ii.) Model: +=1 incorrect letters
+        // iii.) check if you've won --> models.update
+        // * Add the letter to Models for guessed letters!
+
+  },
+
+  // checks if key up is a valid key or not
+  isValidGuess: function(userGuess){
+
+  },
+  // checks if the guess is in word or not
+  processGuess: function(userGuess){
+
+  },
+
+}
 
 // Make an Event Listener that will wait  
 // 2 ways to make an event listener
@@ -119,7 +170,7 @@ document.addEventListener("keyup", function(event){
     // 3b.) if its not correct --> update view
     console.log(userGuess + " is NOT in the currentWord: " + model.currentWord);
     // update user total guesses, only for incorrect guesses!
-    model.totalGuesses +=1;
+    model.incorrectGuesses +=1;
   };
   // *Always excute below code, since we have been given 
   // a valide new guess!
@@ -127,9 +178,9 @@ document.addEventListener("keyup", function(event){
     // update user guesses to model
     model.lettersGuessed.push(userGuess);
     // console.log(model.lettersGuessed);
-    // console.log(model.totalGuesses);
+    // console.log(model.incorrectGuesses);
     // And update the guess count
-    console.log("You have: " + (model.guessesAllowed - model.totalGuesses) + " guesses left.");
+    console.log("You have: " + (model.guessesAllowed - model.incorrectGuesses) + " guesses left.");
   })();
 
 
