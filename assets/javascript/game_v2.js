8fefef3aa9f227b1086c849bf5d8da11d29ba3d9
thechutrain <chu.chuyalan@gmail.com>
2016-11-13 18:42:21 -0500

    have working everything, basically need to just write a check if you won function

--- assets/javascript/game_v2.js
+++ assets/javascript/game_v2.js
@@ -6,9 +6,14 @@ View
 2.) send message to user
 */
 var view = {
+  //Properties
   messageElement: document.getElementById("messages"),
+  // Methods
   updateMessage: function(messageString){
-   this.messageElement.innerHTML = messageString;
+    this.messageElement.innerHTML = messageString;
+    // have the message disappear after 5 seconds!
+    // setTimeout(function(){}, 5000);
+    // console.log("Hi");
   }
 }
 
@@ -30,7 +35,7 @@ var model = {
   freezeGame: false,
   userWon: false,
   currentWord: "John Adams",
-  currentWordArray: [],
+  currentWordArray: ["j", "o", "h", "n", "=", "a", "d","a","m", "s"],
   lettersGuessed: [],
   // incorrectLetters: ["b", "c"],
   // correctLetters: [],
@@ -59,30 +64,34 @@ var model = {
   },
 
   isValidGuess: function(event){
-    // return true if valid guess
-    // returns false if guess is not valid
+    // return an array:
+    // [boolean of whether is was valid or not, "Error Message"]
     // 1.) check that its a letter
     if (event.keyCode < 65 || event.keyCode > 90){
       view.updateMessage(event.key + " is not a letter");
-      return false;
+      return [false, "Not a letter"];
     } 
     // 2.) check that it has not been guessed yet
     else if (this.lettersGuessed.indexOf(event.key) >= 0){
       view.updateMessage(event.key + " was ALREADY guessed!");
-      return false;
+      return [false, "Already Guessed"];
     }
     // assume its a valid guess otherwise
-    return true;
+    return [true, "Valid Guess"];
   },
 
   // checks if the guess is in word or not
-  processGuess: function(event){
-    // all letter to the letters guessed
-
-    // if letter is in word, return the letter
-
-    // if letter is not in the word, return 
-
+  processGuess: function(userGuess){
+    // returns the letter(str) if its found, else undefined
+    // console.log("Inside model.processGuess: " + userGuess);
+    // 1.) add letter to the letters guessed
+    this.lettersGuessed.push(userGuess);
+    // 2.) if letter is in word, return the letter
+    if (this.currentWordArray.indexOf(userGuess) >= 0){
+      return true; // this will be a string
+    }
+    // 3.) if letter is not in the word, return flase
+    return false; 
   },
 
 }
@@ -97,28 +106,40 @@ Controller
 var controller = {
   // This takes an keyup event
   keyupEventListener: function(event){
-    // --- DEBUGGING ---
-    // console.log(event.keyCode);
-    console.log(event.key);
+    // get the user's input!!
+    var userGuess = event.key;
 
     //1.) check to see if the game is done / frozen (function will update! )
     if (model.isGameDone()){
       // if user won, update view
       if (model.userWon){
+        console.log("You won, congrats");
         view.updateMessage("You won, congrats");
       } else{
+        console.log("Better luck next time kiddo!");
         view.updateMessage("Better luck next time!");
       }
+      // Do Not Proceed!
+      return;
     }
 
     //2.) check to see if the user input is valid
-    if (!model.isValidGuess(event)){
-      // don't continue!!
-      console.log("that's not a valid letter");
-      return false;
+    var validResults = model.isValidGuess(event);
+    // console.log(validResults); // array[boolean if valid, error message]
+    if (!validResults[0]){
+      view.updateMessage(validResults[1]);
+      // Do Not Proceed!
+      return;
     }
 
     //3.) process the guess
+    if(model.processGuess(userGuess)){
+      // the user had a correct vote!
+      view.updateMessage(userGuess + " is a correct guess!");
+    } else{
+      view.updateMessage("Sorry, no " + userGuess);
+    }
+
   },
 } //closes controller
 
