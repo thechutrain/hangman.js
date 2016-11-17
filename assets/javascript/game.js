c5ad2ccf70fdc31e1b6b9d05eecbba564aed8ca4
thechutrain <chu.chuyalan@gmail.com>
2016-11-17 11:45:39 -0500

    cleaning up javascript files, since I'll only be working with game_v2 which is now just game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,106 @@
-// console.log("This is a test!");
+/*
+============================================================
+View
+============================================================
+1.) will update the keyboard GUI
+2.) send message to user
+*/
+var view = {
+  //Properties
+  messageElement: document.getElementById("messages"),
+
+  // Methods
+  updateMessage: function(messageString){
+    this.messageElement.innerHTML = messageString;
+    // have the message disappear after 5 seconds!
+    // setTimeout(function(){}, 5000);
+    // console.log("Hi");
+  },
+
+  updateStats: function(){
+
+  },
+
+  updateWord: function(wordArray){
+    // this function will be given the length of the word &
+    // an array of the word to display
+    // ex.) __ __ __   __ __ __ __ __
+    // 1) get the parent node
+    var container = document.getElementById("wordBody");
+
+    // 2.) get the child node
+    var oldWrapper = document.getElementById("wordWrapper");
+
+    // 3.) make a new child node that will replace the original
+    // make sure it has same attributes
+    var newWrapper = document.createElement("div"); 
+    newWrapper.setAttribute("id", "wordWrapper");
+    // newWrapper.setAttribute("class", "v4");
+
+    // 3a.) make cookie cutter of letter or space nested element
+    var makeBox = function(id_num, letter_bool, optional_letter){
+      var text, childClass, parentClass;
+      // console.log(optional_letter);
+      // debugger
+      if(letter_bool){
+        text = " ";
+        childClass = "letter";
+        parentClass = "letter-wrapper";
+      } else{
+        text = "//";
+        childClass = "space";
+        parentClass = "no-letter-wrapper";
+      }
+      // Now make it!
+       // create grandchild
+        // * make element, set attributes, make text node, append text node
+        var grandChild = document.createElement("p");
+        grandChild.setAttribute("id", id_num.toString());
+        // check to see if there's a letter:
+        if(optional_letter){
+          var letterText = document.createTextNode(optional_letter);
+        } else{
+          var letterText = document.createTextNode(text);
+        }
+        grandChild.appendChild(letterText);
+
+        // create child
+        // * make the child element, set class, and append child to it
+        var child = document.createElement("div");
+        child.setAttribute("class", childClass);
+        child.appendChild(grandChild);
+
+        // create parent
+        var parent = document.createElement("div");
+        parent.setAttribute("class", parentClass);
+        parent.appendChild(child);
+
+        // return the box!
+        return parent;
+    };
+
+    // 3c.) loop through the length of the array, and if its not a "//" (space)
+    // make a letter element box
+      for (var i=0; i < wordArray.length; i++){ 
+        var element;
+        // check to see if index is a value in spaces(array)
+        if (wordArray[i]=="//"){
+          // should be a space
+          element = makeBox(i, false);
+        } else if(wordArray[i]==" "){
+          // should be empty letter
+          element = makeBox(i, true);
+        } else {
+          // display the letter
+          element = makeBox(i, true, wordArray[i]);
+        }
+        // In both cases, I'll append the newely made element into wrapper element
+        newWrapper.appendChild(element);
+      }
+    // 4.) replace the old child with new child
+    container.replaceChild(newWrapper, oldWrapper);
+  },
+}
 
 /*
 ============================================================
@@ -11,199 +113,229 @@ Model
 */
 var model = {
   // PROPERTIES
-  totalWins: 1,
-  totalLoses: 0,
+  // create 
+  // gameData: {
+  //   totalWins: 0,
+  //   totalLoses: 0,
+  //   totalRounds: 0,
+  //   famousPeople: ["Albert Einstein", "John F Kennedy"],
 
+  // },
   incorrectGuesses: 0,
   guessesAllowed: 7,
   freezeGame: false,
   userWon: false,
-
-
-  currentWord: "John Adams",
+  currentWord: "John F Kennedy",
   currentWordArray: [],
+  userWordArray: [],
   lettersGuessed: [],
-  // incorrectLetters: ["b", "c"],
-  // correctLetters: [],
 
 
   // METHODS
-  guessesLeft: function(){
-    console.log(this.guessesAllowed - this.incorrectGuesses);
+  arrayifyWord: function(){
+    // takes the current word, and creates two arrays
+    // for userView and currentWordArray
+    // console.log(this); // returns model!
+    var word = this.currentWord;
+    // console.log(this.currentWord);
+    var letter;
+    for (var i=0; i< word.length; i++){
+      letter = word[i];
+      if (letter==" "){
+        this.currentWordArray.push("//");
+        this.userWordArray.push("//");
+      } else{
+        // nameArray.push("");
+        this.currentWordArray.push(letter.toUpperCase());
+        this.userWordArray.push(" ");
+      }
+    }
   },
 
-  // checks to see if the game is over or not 
+  newGame: function(){
+    console.log("Loading a new game!!");
+    // this function resets the model, and calls a function to get a random word
+    this.currentWordArray = [];
+    this.userWordArray = [];
+    this.arrayifyWord();
+    // Reset incorrect guesses & letters guessed duhhhh
+    this.incorrectGuesses = 0;
+    this.lettersGuessed = [];
+  },
+
+  didUserWin: function(){
+    // function returns true if user won
+    var array = this.userWordArray;
+    for (var i=0; i < array.length; i++){
+      if(array[i] == " "){
+        return false;
+      }
+    }
+    return true;
+  },
+
   isGameDone: function(){
-    if ((this.guessesAllowed - this.incorrectGuesses) == 0){
+    // returns true if User is out of guesses or if user has won
+    // returns false if the game is not done (!^)
+    if (this.guessesAllowed <= this.incorrectGuesses){
       this.freezeGame = true;
-    };
-    // TO DO: check if user won game or not
-    if (false){
+      return true;
+    }
+    else if (this.didUserWin()){
       this.freezeGame = true;
       this.userWon = true;
+      return true;
+    }
+    // user still has guesses left and has not won yet!
+    return false;
+  },
+
+  isValidGuess: function(event){
+    // return an array:
+    // [boolean of whether is was valid or not, "Error Message"]
+    // 1.) check that its a letter
+    if (event.keyCode < 65 || event.keyCode > 90){
+      view.updateMessage(event.key + " is not a letter");
+      return [false, "Not a letter"];
+    } 
+    // 2.) check that it has not been guessed yet
+    else if (this.lettersGuessed.indexOf(event.key.toUpperCase()) >= 0){
+      view.updateMessage(event.key + " was ALREADY guessed!");
+      return [false, "Already Guessed"];
+    }
+    // assume its a valid guess otherwise
+    return [true, "Valid Guess"];
+  },
+
+  // checks if the guess is in word or not
+  processGuess: function(userGuess){
+    // returns the letter(str) if its found, else undefined
+    // console.log("Inside model.processGuess: " + userGuess);
+    // 1.) add letter to the letters guessed
+    var userGuessUpper = userGuess.toUpperCase();
+    this.lettersGuessed.push(userGuessUpper);
+    // 2.) if letter is in word, return the letter
+    if (this.currentWordArray.indexOf(userGuessUpper) >= 0){
+      console.log(userGuessUpper + " was found!");
+      // Comment to self: maybe I should simplify names ot 
+      // arrayKey and arrayUser ...
+      // Since indexOf only finds the first occurance, loop
+      // through arrayKey and find all matches of the correct guesses
+      // and set those index hits to arrayUser
+      var arrayKey = this.currentWordArray;
+      var arrayUser = this.userWordArray
+      for (var i = 0; i < arrayKey.length; i++){
+        if(arrayKey[i]==userGuessUpper){
+          arrayUser[i]=userGuessUpper;
+        }
+      }
+      return true; // this will be a string
+    }
+    else{
+    // 3.) if letter is not in the word, +=1 incorrect guess, and return false
+    this.incorrectGuesses +=1;
+    return false; 
     }
   },
 
 }
-
-/*
-============================================================
-View
-============================================================
-1.) will update the keyboard GUI
-2.) send message to user
-*/
-var view = {
-  messageElement: document.getElementById("messages"),
-  updateMessage: function(messageString){
-   this.messageElement.innerHTML = messageString;
-  }
-}
-
 
 /*
 ============================================================
 Controller
 ============================================================
-1.) Will contain the event listeners
-2.) Will tell the model to update itself
-3.) Will tell the view to update itself
+1.) will update the keyboard GUI
+2.) send message to user
 */
 var controller = {
-  // This takes an keyup event
+  newGame: function(){
+    model.newGame();
+    view.updateWord(model.userWordArray);
+  },
+
+  // this function checks to see if you should continue or not
+  continue: function(){
+    //1.) check to see if the game is done / frozen (function will update! )
+    if (model.isGameDone()){
+      // if user won, update view
+      if (model.userWon){
+        console.log("You won, congrats");
+        view.updateMessage("You won, congrats");
+      } else{
+        console.log("Better luck next time kiddo!");
+        view.updateMessage("Better luck next time!");
+      }
+      // DO NOT PROCEED because the game is done
+      return false;
+    }
+    return true;
+  },
+
+  test: function(){
+    // console.log("Event listener succesfully calls controller method!");
+    // 1.) Tell the model to start a new game
+    model.newGame();
+    // 1a.) also tell the user that there's been a new word:
+    view.updateMessage("New Game - button was pressed!!");
+    // 2.) Update the view, specifically the word container / display
+    view.updateWord(model.userWordArray);
+    // DEBUGGING
+    // console.log("The new game button was pressed");
+
+  },
+
+  // This takes a keyup event, and will process the key input
   keyupEventListener: function(event){
-    //* 0.) update game & get the pressed key
-    // models.update()
+    // debugging purposes
+    // console.log("Inside keyupEvent Listener!");
+    // get the user's input!!
 
-    // 1.) if game is frozen
-      // 1a.) if player won 
-        //--> update View (You won)
-
-      // 1b.) else if player did not win 
-        // --> update View (You lost)
-
-      // *EXIT!
-
-    // 2) if not valid guess
-      // 2a.) if not a letter
-          // --> update View (Not a letter)
-      // 2b.) if already picked letter
-          // --> update View (Already picked) 
-
-    // 3) else, its a valid guess and process it!
-      // 3a.) if processed guess is right!
-        // i.) update View (That's a correct letter)
-        // ii.) check if you've won --> models.update
-      // 3b.) if processed guess is incorrect
-        // i.) update View (That's a wrong letter)
-        // ii.) Model: +=1 incorrect letters
-        // iii.) check if you've won --> models.update
-        // * Add the letter to Models for guessed letters!
-
-  },
-
-  // checks if key up is a valid key or not
-  isValidGuess: function(userGuess){
-
-  },
-  // checks if the guess is in word or not
-  processGuess: function(userGuess){
-
-  },
-
-}
-
-// Make an Event Listener that will wait  
-// 2 ways to make an event listener
-// document.onkeyup = function(event){
-//   console.log(event.key);
-// };
-
-document.addEventListener("keyup", function(event){
-  // Part 0: get what the user pressed!
-  // get the key that the user pressed
-  // console.log(event.key);
-  var userGuess = event.key;
-
-  // Part1: Check to see if the game is over
-  // TO DO: FUNCTIONALIZE THIS later!!
-  model.isGameDone();
-  if (model.freezeGame){
-    // User already won?
-    if (model.userWon){
-        console.log("You won! Play again?");
-      // TO DO: Play again?
-      // write code if user presses enter, you reset game!
-      return;
-    } else{
-      console.log("Game is over, try again?");
-      // TO DO: Play again?
-      // write code if user presses enter, you reset game!
+    // MAKE INTO A FUNCTION
+    //1.) check to see if the game is done / frozen (function will update! )
+    if (this.continue() == false){
       return;
     }
-  }
 
-
-
-// Part 2: GAME is not over, so lets check what the user guessed!
-  // 1.) check that its a letter
-  if (event.keyCode < 65 || event.keyCode > 90){
-    console.log("Sorry, " + event.key +" is not a valid letter");
-    // TO DO: alert user to only pick letters :)
-    return;
-  } 
-  // else{
-  //   console.log(event.key + " is a letter!");
-  // }
-
-  // 2.) check that it has not been guessed yet
-  if (model.lettersGuessed.indexOf(userGuess)!=-1){
-    console.log(userGuess + " has already been guessed");
-    return;
-  };
-
-  // 3.) check if it's correct
-    // 3a.) if correct --> update view, & update game
-  if (model.currentWord.indexOf(userGuess)!=-1){
-    console.log(userGuess + " is in the currentWord: " + model.currentWord);
-  } else{
-    // 3b.) if its not correct --> update view
-    console.log(userGuess + " is NOT in the currentWord: " + model.currentWord);
-    // update user total guesses, only for incorrect guesses!
-    model.incorrectGuesses +=1;
-  };
-  // *Always excute below code, since we have been given 
-  // a valide new guess!
-  (function(){
-    // update user guesses to model
-    model.lettersGuessed.push(userGuess);
-    // console.log(model.lettersGuessed);
-    // console.log(model.incorrectGuesses);
-    // And update the guess count
-    console.log("You have: " + (model.guessesAllowed - model.incorrectGuesses) + " guesses left.");
-  })();
-
-
-
-  // Part 3 or Part 1 AGAIN: Check to see if the game is over
-  // TO DO: FUNCTIONALIZE THIS!!
-  model.isGameDone();
-  if (model.freezeGame){
-    // User already won?
-    if (model.userWon){
-        console.log("You won! Play again?");
-      return;
-    } else{
-      console.log("Game is over, try again?");
+    //2.) check to see if the user input is valid
+    var validResults = model.isValidGuess(event);
+    // console.log(validResults);
+    // debugger
+    // console.log(validResults); // array[boolean if valid, error message]
+    if (!validResults[0]){
+      view.updateMessage(validResults[1]);
+      // Do Not Proceed!
       return;
     }
-  }
-    
+
+    //3.) process the guess
+    var userGuess = event.key;
+    if(model.processGuess(userGuess)){
+      // the user had a correct vote!
+      view.updateMessage(userGuess + " is a correct guess!");
+      view.updateWord(model.userWordArray);
+    } else{
+      view.updateMessage("Sorry, no " + userGuess);
+    }
 
 
-})
+    // MAKE INTO A FUNCTION
+    if (this.continue() == false){
+      return;
+    }
+
+  },
+} //closes controller
 
 
+
+/*
+============================================================
+Set up the event listener!!
+============================================================
+*/
+window.addEventListener("load", controller.newGame.bind(controller));
+document.getElementById("newGame").addEventListener("click", controller.test);
+document.addEventListener("keyup", controller.keyupEventListener.bind(controller));
 
 
 
