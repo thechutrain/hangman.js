8e67dde9512f4f2726fc7553731ba63dac41b7ab
thechutrain <chu.chuyalan@gmail.com>
2016-11-22 13:49:29 -0500

    working view.displayFact method

--- assets/javascript/game_v2.js
+++ assets/javascript/game_v2.js
@@ -4,7 +4,7 @@ View
 ============================================================
 */
 var view ={
-  // Properties - DOM variables
+  // view - Properties, all are DOM variables
   mapBox: document.querySelector("#worldMap"),
   factBox: document.querySelector("#factBox"),
   // statsBox: document.querySelector("#statsBox"),
@@ -14,14 +14,75 @@ var view ={
   questionWrapper: document.querySelector("#questionBox"),
   wordWrapper: document.querySelector("#wordBox"),
 
+  // View - methods
+  displayMap: function(countryNameStr){
+    // takes a country name string and updates view so that it will
+    // highlight that country. If no string, displays no country
+  },
 
-}
+  displayFact: function(factsArray){
+    // takes an array of facts, and displays each one
+    // as a bullet point 
+    // 1.) clear previous HTML
+    while(this.factBox.firstChild){
+      this.factBox.removeChild(this.factBox.firstChild);
+    }
+    // 2.) create the update container
+    var update = document.createElement("ul");
+    // 3.) loop through each fact in the array
+    for (var i=0; i< factsArray.length; i++){
+      //3a.) create a new li element
+      var li = document.createElement("li");
+      //3b.) create a new text node for that li element
+      //3c.) update that text to the li element
+      li.innerHTML = factsArray[i];
+      //3d.) append the whole li element to the ul
+      update.appendChild(li);
+    }
+    // 4.) append new update
+    this.factBox.appendChild(update);
+  },
 
-// testing view
-for (x in view){
-  console.log(view[x]);
-  console.log("--------");
-}
+  displayGuessesLeft: function(guessLeftNumber){
+    // takes a number and displays the guesses left
+  },
+
+  displayKeyboard: function(correctArray, incorrectArray){
+    // takes in 2 arrays of correct letters & incorrect letters
+    // if none in both, sets everything to default
+
+  },
+
+  displayMessage: function(messageString, warningBoolean){
+    // will display either a success message or warning message
+  },
+
+  displayQuestion: function(questionString){
+    // will display the question for the specific country
+
+  },
+
+  displayWord: function(wordString){
+    // will display the the current word
+  },
+
+
+} // closes View object
+
+// TESTING VIEW
+// for (x in view){
+//   console.log(view[x]);
+//   console.log("--------");
+// }
+
+// TESTING VIEW
+// view.displayFact(["awesome fact 1", "another awesome fact"]);
+
+
+
+
+
+
 
 /*
 ============================================================
